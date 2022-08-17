2337829568dce9420c1d1be921710326962e2f12
Matichiusa123 <107504929+Matichiusa123@users.noreply.github.com>
2022-08-17 14:46:49 -0300

    Initial commit

---
+++ archivo.js
@@ -0,0 +1,38 @@
+//CONSIGNA 1
+ /*
+for(let i=0; i < 10; i=i++){
+    let numero = parseInt(prompt("Ingrese un número y te voy a mostrar el siguiente"));
+    console.log("Ingresaste el numero:", numero);
+    console.log("El siguiente numero es:", numero + 1);
+}
+*/
+
+
+//CONSIGNA 2
+
+/*
+let nombre = "";
+let edad = "";
+let email = "";
+
+while (nombre, edad , email != "ESC"){ 
+    nombre = prompt ("Ingrese su nombre o ESC para finalizar");
+    edad = prompt ("Ingrese su edad o ESC para salir");
+    email = prompt ("Ingrese su email o ESC para salir");
+
+    console.log ("Bienvenido/a", nombre);
+    console.log ("Su edad es:", edad);
+    console.log("Su email es: ", email);
+}
+*/
+
+//CONSIGNA 3
+
+/*
+for(let i = 0; i <= 2 ; i = i + 1){
+let mensaje = prompt("Mensaje de bienvenida")
+let numero = parseInt(prompt("Ingrese un numero"));
+console.log("Hola:", mensaje);
+console.log("Tu numero es:", numero);
+}
+
